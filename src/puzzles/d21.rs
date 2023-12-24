393f850e6c7e1d5c314eeca631a0174fb0cabbf0
Diederik Perdok <diederik.perdok@ah.nl>
2023-12-24 20:56:13 +0100

    Optimize part 1 performance

--- src/puzzles/d21.rs
+++ src/puzzles/d21.rs
@@ -1,6 +1,7 @@
 use std::{error::Error, str::FromStr};
 
 use ndarray::{Array, Array2};
+use rustc_hash::FxHashMap as HashMap;
 use rustc_hash::FxHashSet as HashSet;
 
 pub struct Garden {
@@ -36,36 +37,62 @@ impl FromStr for Garden {
 
 impl Garden {
     pub fn num_tiles_reacheable_after(&self, n_steps: u32) -> u32 {
-        let mut reacheable = HashSet::default();
-        reacheable.insert(self.start_pos);
+        // Keep track of after how many steps a position was first reached in
+        // `reached_after`. If a position was reached after n steps, it will be
+        // reached again after n + 2, n + 4, n + 6, etc.
+        let mut reached_after = HashMap::default();
+        reached_after.insert(self.start_pos, 0);
 
-        for _ in 0..n_steps {
-            let mut new_reacheable = HashSet::default();
-            for pos in reacheable.into_iter() {
-                new_reacheable.extend(self.neighbours(&pos));
+        let mut curr_positions = HashSet::default();
+        curr_positions.insert(self.start_pos);
+
+        for n in 0..n_steps {
+            let mut next_positions = HashSet::default();
+            for pos in curr_positions.into_iter() {
+                next_positions.extend(
+                    self.neighbours(&pos)
+                        .filter(|pos| !reached_after.contains_key(pos)),
+                );
             }
-            reacheable = new_reacheable;
+
+            for pos in &next_positions {
+                reached_after.insert(*pos, n + 1);
+            }
+            curr_positions = next_positions;
         }
 
-        u32::try_from(reacheable.len()).unwrap()
+        // if n_steps is even, we can reach all positions we reached after an
+        // even number of steps.
+        u32::try_from(
+            reached_after
+                .into_iter()
+                .filter(|&(_pos, steps)| steps % 2 == n_steps % 2)
+                .count(),
+        )
+        .unwrap()
     }
 
-    fn neighbours(&self, pos: &(usize, usize)) -> impl Iterator<Item = (usize, usize)> + '_ {
+    fn neighbours<'a>(&'a self, pos: &(usize, usize)) -> impl Iterator<Item = (usize, usize)> + 'a {
         let pos = (i32::try_from(pos.0).unwrap(), i32::try_from(pos.1).unwrap());
+        let height = i32::try_from(self.grid.shape()[0]).unwrap();
+        let width = i32::try_from(self.grid.shape()[1]).unwrap();
 
         [(0, 1), (-1, 0), (0, -1), (1, 0)]
             .into_iter()
-            .map(move |(dy, dx)| {
+            .map(move |(dy, dx)| (pos.0 + dy, pos.1 + dx))
+            .filter(move |&neighbour_pos| {
+                neighbour_pos.0 >= 0
+                    && neighbour_pos.0 < height
+                    && neighbour_pos.1 >= 0
+                    && neighbour_pos.1 < width
+            })
+            .map(|neighbour_pos| {
                 (
-                    usize::try_from(pos.0 + dy).unwrap(),
-                    usize::try_from(pos.1 + dx).unwrap(),
+                    usize::try_from(neighbour_pos.0).unwrap(),
+                    usize::try_from(neighbour_pos.1).unwrap(),
                 )
             })
-            .filter(|&neighbour_pos| {
-                self.grid
-                    .get(neighbour_pos)
-                    .is_some_and(|&tile| tile != b'#')
-            })
+            .filter(|&neighbour_pos| self.grid[neighbour_pos] != b'#')
     }
 }
 
